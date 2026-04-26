25f24943e269bd2e49df2419214e63c2c13a3e57
Egyptology <winchsim@gmail.com>
2026-04-26 14:04:07 +0300

    Fixed mistakes

--- BlockChain2/obj/Debug/net8.0/BlockChain2.AssemblyInfo.cs
+++ BlockChain2/obj/Debug/net8.0/BlockChain2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlockChain2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ea0f9e2f120455354073db8b909ce1d5c709f3f")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlockChain2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlockChain2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
